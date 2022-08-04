32be024f3e3dfb2fd54a54e5ea85e4f2995f482f
Joshua Baker <jbaker264@yahoo.com>
2022-08-04 15:06:00 +0000

    Completed Issue 1 User can View Data and Choose Day

--- js/data.js
+++ js/data.js
@@ -1,1 +1,19 @@
 /* exported data */
+var data = {
+  view: 'home-page',
+  entries: [],
+  nextEntryId: 1
+};
+
+var previousDataJson = localStorage.getItem('ajax-local-storage');
+
+if (previousDataJson !== null) {
+  data = JSON.parse(previousDataJson);
+}
+
+function storeItems(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('ajax-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', storeItems);
